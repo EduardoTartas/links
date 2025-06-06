c8cdaf3f72554a2d1b3a0b794b58225dd0d37d31
EduardoTartas <eduardos.tartas@gmail.com>
2025-06-05 22:11:48 -0400

    ajustes

--- src/main.ts
+++ src/main.ts
@@ -1,68 +1,97 @@
-import './assets/styles/index.css';
+import "./assets/styles/index.css";
 
-const container = document.querySelector<HTMLDivElement>('#container')!;
-const profilePicture = document.querySelector<HTMLImageElement>('#profilePicture')!;
-const profileName = document.querySelector<HTMLHeadingElement>('#profileName')!;
-const qrCode = document.querySelector<HTMLImageElement>('#qrCode')!;
-const link = document.querySelector<HTMLAnchorElement>('.link')!;
-
+const container = document.querySelector<HTMLDivElement>("#container")!;
+const profilePicture =
+  document.querySelector<HTMLImageElement>("#profilePicture")!;
+const profileName = document.querySelector<HTMLHeadingElement>("#profileName")!;
+const qrCode = document.querySelector<HTMLImageElement>("#qrCode")!;
+const allLinks = document.querySelectorAll<HTMLAnchorElement>(".link");
+const icons = document.querySelectorAll<SVGElement>(".icon");
 
 const params = new URLSearchParams(window.location.search);
-const id = params.get('id');
+const id = params.get("id");
 
 async function getUserData() {
-    if (id){
+  if (id) {
     const response = await fetch(`http://localhost:3000/users/${id}`);
     const data = await response.json();
     console.log(data);
     return data;
-    }
-    else{
-        alert('No user ID provided in the URL.');
-    }
+  } else {
+    alert("No user ID provided in the URL.");
+  }
 }
 
 const data = await getUserData();
 
 if (data) {
-  profilePicture.src = data.profilePicture || 'default-profile.jpg';
-  profileName.textContent = data.name || 'User';
-  qrCode.src = data.QRcode || 'default-qr.png';
-  
-  if(data.background){
-    const image = data.background.find((item: { image?: string }) => 'image' in item)?.image;
-    const color = data.background.find((item: { color?: string }) => 'color' in item)?.color;
+  profilePicture.src = data.profilePicture || "default-profile.jpg";
+  profileName.textContent = data.name || "User";
+  qrCode.src = data.QRcode || "default-qr.png";
 
-    container.style.backgroundImage = image ? `url(${image})` : 'none';
+  if (data.background) {
+    const image = data.background.find(
+      (item: { image?: string }) => "image" in item
+    )?.image;
+    const color = data.background.find(
+      (item: { color?: string }) => "color" in item
+    )?.color;
+
+    container.style.backgroundImage = image ? `url(${image})` : "none";
     container.style.boxShadow = `inset 0 0 0 2000px ${color}`;
-    
   }
-
 
   //estilo do link
   if (data.linkStyle) {
+    const defaultColor = data.linkStyle.find(
+      (item: { defaultColor?: string }) => "defaultColor" in item
+    )?.defaultColor;
+    const hoverColor = data.linkStyle.find(
+      (item: { hoverColor?: string }) => "hoverColor" in item
+    )?.hoverColor;
+    const linkColor = data.linkStyle.find(
+      (item: { linkColor?: string }) => "linkColor" in item
+    )?.linkColor;
+    const borderRadius = data.linkStyle.find(
+      (item: { borderRadius?: string }) => "borderRadius" in item
+    )?.borderRadius;
 
-    const defaultColor = data.linkStyle.find((item: { defaultColor?: string }) => 'defaultColor' in item)?.defaultColor;
-    const hoverColor = data.linkStyle.find((item: { hoverColor?: string }) => 'hoverColor' in item)?.hoverColor;
-    const linkColor = data.linkStyle.find((item: { linkColor?: string }) => 'linkColor' in item)?.linkColor;
-    const borderRadius = data.linkStyle.find((item: { borderRadius?: string }) => 'borderRadius' in item)?.borderRadius;
-    
     qrCode.style.backgroundColor = defaultColor;
 
-    const allLinks = document.querySelectorAll<HTMLAnchorElement>('.link');
-    
-    allLinks.forEach(linkElement => {
+    allLinks.forEach((linkElement) => {
       linkElement.style.backgroundColor = defaultColor;
       linkElement.style.color = linkColor;
       linkElement.style.borderRadius = borderRadius;
     });
-    
-    const style = document.createElement('style');
-    style.textContent = `
+
+    if (linkColor == "#010101") {
+      icons.forEach((icon) => {
+        icon.style.filter = "brightness(0) invert(0)";
+      });
+
+      const style = document.createElement("style");
+      style.textContent = `
+       .link:hover {
+      background-color: ${defaultColor} !important;
+       }
+
+       .link:hover .icon {
+      filter: invert(100%) !important;
+       }
+     `;
+      document.head.appendChild(style);
+    } else {
+      const style = document.createElement("style");
+      style.textContent = `
       .link:hover {
-        background-color: ${hoverColor} !important;
-      }
+      background-color: ${hoverColor} !important;
+      },
+
+      .link:hover .icon {
+      filter: invert(100%) !important;
+      },
     `;
-    document.head.appendChild(style);
+      document.head.appendChild(style);
+    }
   }
 }
